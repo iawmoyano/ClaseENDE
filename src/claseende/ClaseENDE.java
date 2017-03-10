518be5db25685c404352698142c681e60633354e
iawmoyano <iaw.moyano@gmail.com>
2017-03-10 14:18:55 +0100

    Añadida línea 2

--- src/claseende/ClaseENDE.java
+++ src/claseende/ClaseENDE.java
@@ -4,6 +4,7 @@ public class ClaseENDE {
 
     public static void main(String[] args) {
         System.out.println("Inicio de proyecto");
+        System.out.println("Inicio de proyecto 2");
     }
     
 }
